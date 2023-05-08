0e94e4d7757a3b7365032357d2fd0e27a2c83b5b
Antonio Yang <yanganto@gmail.com>
2023-05-08 22:46:17 +0800

    lint

--- lib/src/traits.rs
+++ lib/src/traits.rs
@@ -5,7 +5,7 @@ pub trait TomlExample {
     /// structure to toml example
     fn toml_example() -> String;
     fn to_toml_example(file_name: &str) -> std::io::Result<()> {
-        let mut file =File::create(file_name)?;
+        let mut file = File::create(file_name)?;
         file.write_all(Self::toml_example().as_bytes())?;
         Ok(())
     }
