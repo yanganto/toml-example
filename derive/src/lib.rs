25e1f6b037eab6d22fff0beb81ef84318132148b
Antonio Yang <yanganto@gmail.com>
2024-09-12 20:49:33 +0800

    update proc-macro-error2 (#27)
    * update proc-macro-error2
    
    * typo

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro_error::{abort, proc_macro_error};
+use proc_macro_error2::{abort, proc_macro_error};
 use quote::quote;
 use syn::{
     AngleBracketedGenericArguments,
