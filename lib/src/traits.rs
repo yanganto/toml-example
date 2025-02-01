87451a511ddf2d233552298b7bef045444cfb8ef
Antonio Yang <yanganto@gmail.com>
2025-02-01 14:05:18 +0800

    allow pass Path to create example

--- lib/src/traits.rs
+++ lib/src/traits.rs
@@ -1,11 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 pub trait TomlExample {
     /// structure to toml example
     fn toml_example() -> String;
     fn toml_example_with_prefix(label: &str, prefix: &str) -> String;
-    fn to_toml_example(file_name: &str) -> std::io::Result<()> {
+    fn to_toml_example<P: AsRef<Path>>(file_name: P) -> std::io::Result<()> {
         let mut file = File::create(file_name)?;
         file.write_all(Self::toml_example().as_bytes())?;
         Ok(())
