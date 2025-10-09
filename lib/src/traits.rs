548f9ac0d72b4ba30d14b0486fa582ed3877803a
Antonio Yang <yanganto@gmail.com>
2025-10-09 09:36:01 +0800

    typo

--- lib/src/traits.rs
+++ lib/src/traits.rs
@@ -7,7 +7,7 @@ pub trait TomlExample {
     fn toml_example() -> String;
 
     /// structure, which is nesting or flatten inside other structure, to a toml example
-    /// There will be a section `{label_format.0}{label}{lable_format.1}` for the example of struct, and `prefix` will add `# ` if it is a optional.
+    /// There will be a section `{label_format.0}{label}{label_format.1}` for the example of struct, and `prefix` will add `# ` if it is a optional.
     fn toml_example_with_prefix(label: &str, label_format: (&str, &str), prefix: &str) -> String;
 
     fn to_toml_example<P: AsRef<Path>>(file_name: P) -> std::io::Result<()> {
